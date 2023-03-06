7635dd5a59d7ec3ce9acae52d205aefb87f34383
Rpcd <67294809+RuiDaniel@users.noreply.github.com>
2023-03-06 14:12:50 +0000

    Add files via upload

---
+++ Makefile
@@ -0,0 +1,24 @@
+TARGET = tsp
+CC = gcc
+CFLAGS = -Wall -O3 -fopenmp
+
+default: $(TARGET)
+all: default
+
+OBJECTS = $(patsubst %.c, %.o, $(wildcard *.c))
+HEADERS = $(wildcard *.h)
+
+%.o: %.c $(HEADERS)
+	$(CC) $(CFLAGS) -c $< -o $@
+
+.PRECIOUS: $(TARGET) $(OBJECTS)
+
+$(TARGET): $(OBJECTS)
+	$(CC) $(OBJECTS) -fopenmp -Wall -o $@
+
+.PHONY: default all clean
+
+clean:
+	-rm -f *.o
+	-rm -f $(TARGET)
+	-rm -f vgcore*
---
+++ auxiliar.h
@@ -0,0 +1,39 @@
+/**********************************************************************************
+* (h) Tomás Fonseca - 66325
+*     Afonso Alemão - 96135
+*     Rui Daniel    - 96317
+*
+* Last Modified: 27/02/2023
+*
+* Name: auxiliar.h
+*
+* Description: auxiliar.c header
+*
+* Comments:
+*
+**********************************************************************************/
+
+#ifndef _AUXILIAR_H
+#define _AUXILIAR_H
+
+#include "structs.h"
+
+/* Methods */
+
+double **aloc2d(int, int);
+char compare (void *, void *);
+double distance(int, int, Inputs *);
+void error();
+void free2d(double **, int);
+void *free_safe (void *);
+void InitializeIsInTour(int *, int);
+double InitialLowerBound(Inputs *);
+double newLowerBound(Inputs *, Path *, int);
+FILE *OpenFile(char *, char *);
+Inputs *parse_inputs(int, char **);
+void print_matrix(int, Inputs *);
+void print_result(Solution *, int);
+Inputs *ReadFileIn(FILE *, double);
+void ValidInputFileName(char *);
+
+#endif
