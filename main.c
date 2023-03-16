b0047036ea95293d04f83c1191a796df3196c576
Rpcd <67294809+RuiDaniel@users.noreply.github.com>
2023-03-16 17:21:33 +0000

    Add files via upload

--- main.c
+++ main.c
@@ -20,6 +20,7 @@
 
 #include "queue.h"
 #include "tsp.h"
+#include "tsp-omp.h"
 #include "auxiliar.h"
 
 /**********************************************************************************
@@ -46,6 +47,7 @@ int main(int argc, char *argv[]) {
     exec_time = -omp_get_wtime();
 
     solution = tsp(input);
+    solution = tsp_omp(input); 
 
     exec_time += omp_get_wtime();
     fprintf(stderr, "%.1fs\n", exec_time);
