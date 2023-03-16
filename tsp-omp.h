d0ba995d27cc7ade5188d2a1124d67078924eef6
Rpcd <67294809+RuiDaniel@users.noreply.github.com>
2023-03-16 17:19:35 +0000

    Add files via upload

---
+++ tsp-omp.h
@@ -0,0 +1,26 @@
+/**********************************************************************************
+* (h) Tomás Fonseca - 66325
+*     Afonso Alemão - 96135
+*     Rui Daniel    - 96317
+*
+* Last Modified: 27/02/2023
+*
+* Name: tsp_omp.h
+*
+* Description: tsp_omp.c header
+*
+* Comments:
+*
+**********************************************************************************/
+
+#ifndef _TSP_OMP_H
+#define _TSP_OMP_H
+
+#include <omp.h>
+#include "auxiliar.h"
+
+Solution *tsp_omp(Inputs *input);
+
+#endif
+
+
