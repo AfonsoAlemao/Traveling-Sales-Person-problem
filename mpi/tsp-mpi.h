cd5ad3d33dbe90abd3e6b7772e96ba6f2887a8f3
Rpcd <67294809+RuiDaniel@users.noreply.github.com>
2023-04-08 18:02:14 +0100

    Add files via upload

---
+++ mpi/tsp-mpi.h
@@ -0,0 +1,29 @@
+/**********************************************************************************
+* (h) Tomás Fonseca - 66325
+*     Afonso Alemão - 96135
+*     Rui Daniel    - 96317
+*
+* Last Modified: 12/04/2023
+*
+* Name: tsp_mpi.h
+*
+* Description: tsp_mpi.c header
+*
+* Comments:
+*
+**********************************************************************************/
+
+#ifndef _TSP_OMP_H
+#define _TSP_OMP_H
+
+#include <omp.h>
+
+#include "auxiliar.h"
+#include "queue.h"
+
+Solution *tsp_omp(Inputs *);
+void work(priority_queue_t *, int , double *, Inputs* , Solution *, Path*, int *);
+
+#endif
+
+
